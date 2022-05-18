8624ab3d8e9812202f58775a0578c0b28e4f03eb
Ty <tdewitt2@gmail.com>
2022-05-17 23:34:23 -0400

    streamlined sorting function; slightly improved form & table layouts

--- moviesApp/app.js
+++ moviesApp/app.js
@@ -36,30 +36,54 @@ $('.sortBtn').on('click', function(event) {
     makeSortedMovieTable(sortMovieTable(event.target.id,$('#sortDir').val()));
 });
 
-function sortMovieTable(field,direction='desc') {
-    const moviesToSort=[];
-    const moviesRearranged=[];
+// function sortMovieTable(field,direction='asc') {
+//     const moviesToSort=[];
+//     const moviesRearranged=[];
+//     if(field==='sortABC') {
+//         movies.forEach((x) => moviesToSort.push([x[1],x[0],x[2]]));
+
+//     } else if (field==='sort123') {
+//         movies.forEach((x) => moviesToSort.push([x[2],x[0],x[1]]));
+//     }
+
+//     if (direction==='asc') {
+//         moviesToSort.sort();
+//     } else if (direction==='desc') {
+//         moviesToSort.sort().reverse();
+//     }
+
+//     if(field==='sortABC') {
+//         moviesToSort.forEach((x) => moviesRearranged.push([x[1],x[0],x[2]]));
+//         return moviesRearranged;
+//     } else if(field==='sort123') {
+//         moviesToSort.forEach((x) => moviesRearranged.push([x[1],x[2],x[0]]));
+//         return moviesRearranged;
+//     }
+// }
+
+//  const sampleMovies = [[0,'it','6.5'],[1,'point break','10'],[2,'Lion king','6'],[3,'amadeus','8'],[4,'parasite','8.5'],[5,'hunt for the wilderpeople','7.5']];
+
+function sortMovieTable(field,direction='asc') {
+    
     if(field==='sortABC') {
-        movies.forEach((x) => moviesToSort.push([x[1],x[0],x[2]]));
-
-    } else if (field==='sort123') {
-        movies.forEach((x) => moviesToSort.push([x[2],x[0],x[1]]));
+        const sortedMovies = movies.sort((a,b) => a[1].toLowerCase()>b[1].toLowerCase() ? 1 : -1);
+        if (direction==='asc') {
+            return sortedMovies;
+        } else if (direction==='desc') {
+            return sortedMovies.reverse();
+        }
     }
 
-    if (direction==='asc') {
-        moviesToSort.sort();
-    } else if (direction==='desc') {
-        moviesToSort.sort().reverse();
+    if(field==='sort123') {
+        const sortedMovies = movies.sort((a,b) => +a[2]>+b[2] ? 1 : -1);
+        if (direction==='asc') {
+            return sortedMovies;
+        } else if (direction==='desc') {
+            return sortedMovies.reverse();
+        }
     }
 
-    if(field==='sortABC') {
-        moviesToSort.forEach((x) => moviesRearranged.push([x[1],x[0],x[2]]));
-        return moviesRearranged;
-    } else if(field==='sort123') {
-        moviesToSort.forEach((x) => moviesRearranged.push([x[1],x[2],x[0]]));
-        return moviesRearranged;
-    }
-
+    return null; //error
 }
 
 
